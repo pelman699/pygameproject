a364f5df59e2e02154d5951df8ea07c28a051bd8
pelman669 <levkerskij@gmail.com>
2021-03-07 16:34:04 +0300

    pelmeni is the bestest

--- main.py
+++ main.py
@@ -1,41 +1,56 @@
-# Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 60
-
-# Задаем цвета
+WIDTH = 800
+HEIGHT = 800
+FPS = 30
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((50,50))
+        self.image.fill(GREEN)
+        self.rect = self.image.get_rect()
+        self.rect.center = (WIDTH/2,HEIGHT/2)
+    def update(self):
+        rx = random.randint(-5, 10)
+        ry = random.randint(-5, 10)
+        self.rect.x += rx
+        self.rect.y += ry
+        if self.rect.right < 0:
+            self.rect.left = WIDTH
+        if self.rect.top > HEIGHT:
+            self.rect.bottom = 0
+        if self.rect.left > WIDTH:
+            self.rect.right = 0
+        if self.rect.bottom < 0:
+            self.rect.top = HEIGHT
+        print(self.rect.x,self.rect.y)
 
-# Создаем игру и окно
+all_sprites = pygame.sprite.Group()
+player = Player()
+all_sprites.add(player)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("My Game")
 clock = pygame.time.Clock()
 
-# Цикл игры
 running = True
 while running:
-    # Держим цикл на правильной скорости
     clock.tick(FPS)
-    # Ввод процесса (события)
     for event in pygame.event.get():
-        # check for closing window
         if event.type == pygame.QUIT:
             running = False
-
-    # Обновление
-
-    # Рендеринг
+    all_sprites.update()
     screen.fill(BLACK)
-    # После отрисовки всего, переворачиваем экран
+    all_sprites.draw(screen)
     pygame.display.flip()
 
+
 pygame.quit()
+
